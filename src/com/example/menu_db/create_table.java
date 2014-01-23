8c2b409f38152eb45cb29f1a5d2736bac83d1a9e
SatyabrataMohanty <dkole@aurustech.com>
2014-01-23 10:09:13 +0530

    SatyabrataMohanty commit

---
+++ src/com/example/menu_db/create_table.java
@@ -0,0 +1,30 @@
+package com.example.menu_db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class create_table extends SQLiteOpenHelper{
+public static final String name="dbname";
+	public create_table(Context context, CursorFactory factory,
+			int version) {
+		super(context, name, null,version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String sql="create table clg_info1(c_id integer primary key autoincrement,c_name text,c_course text,c_university)";
+		db.execSQL(sql);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
